d136cee7776aedbbe5911397cc5e694c9f873b00
sajithra-uki <sajithra.uki.stu@dreamspace.academy>
2025-12-17 17:07:12 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const http = require('http');
 const express= require('express');
 
+
+const bodyparser= require('body-parser');
 const  app =express();
 
-app.use((req,res,next)=>{
+app.use(bodyparser.urlencoded());
+
+app.use('/',(req,res,next)=>{
     console.log("this is first middleware");
-    res.send("<h1>Hi guys good morning!</h1>")
+    res.send("<h1>Hi guys good morning!</h1>");
+    next();
 });
-app.use((req,res,next)=>{
+app.use('/add-student',(req,res,next)=>{
+    console.log("this is secound middleware");
+    res.send("<h1>welcome!</h1>");
+    next();
+});
+pp.use('/added-student',(req,res,next)=>{
     console.log("this is secound middleware");
     res.send("<h1>welcome!</h1>")
 });
